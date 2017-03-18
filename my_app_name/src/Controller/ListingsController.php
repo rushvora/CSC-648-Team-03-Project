099376e1049141a90d8cea1d9ac5e3fb64b8bfa1
Mark Reynolds <mbreynolds147@gmail.com>
2017-03-18 03:20:24 +0000

    Add ListingsController skeleton
    Added ListingsController skeleton code with documentation.

---
+++ my_app_name/src/Controller/ListingsController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Controller for listing.
+ */
+class ListingsController extends AppController
+{
+
+  /**
+   * Action for viewing a listing.
+   *
+   * @param id The id of the listing to view.
+   *
+   * @post Set a string for listingName, listingDescription, listingImage.
+   */
+  public function view($id)
+  {
+    //Do stuff.
+  }
+
+  /**
+   * Action for searching for a listing.
+   *
+   * @param query The text to search for.
+   * @param category The category to search in.
+   *
+   * @post Set an array for results. Each result should be an array containing strings for listingName, listingShortDescription, listingImage.
+   */
+  public function search($query, $category)
+  {
+    //Do stuff.
+  }
+
+}
+
