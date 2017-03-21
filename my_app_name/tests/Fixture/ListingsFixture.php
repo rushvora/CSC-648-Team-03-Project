0142bb85d0c55a1d81e514fa1eea378496129072
--quiet <sp17g03@ip-172-31-19-160.us-west-1.compute.internal>
2017-03-21 02:38:20 +0000

    Pushed updated model with DB connected to framework - Rushabh.

---
+++ my_app_name/tests/Fixture/ListingsFixture.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ListingsFixture
+ *
+ */
+class ListingsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'ListingsId' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'Name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => 'Name of the listing.', 'precision' => null, 'fixed' => null],
+        'Price' => ['type' => 'decimal', 'length' => 8, 'precision' => 2, 'unsigned' => false, 'null' => true, 'default' => null, 'comment' => ''],
+        'Seller' => ['type' => 'string', 'length' => 100, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Image' => ['type' => 'binary', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'Category' => ['type' => 'string', 'length' => 45, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'Pick Up Location' => ['type' => 'string', 'length' => 200, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['ListingsId'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'latin1_swedish_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'ListingsId' => 1,
+            'Name' => 'Lorem ipsum dolor sit amet',
+            'Price' => 1.5,
+            'Seller' => 'Lorem ipsum dolor sit amet',
+            'Image' => 'Lorem ipsum dolor sit amet',
+            'Category' => 'Lorem ipsum dolor sit amet',
+            'Pick Up Location' => 'Lorem ipsum dolor sit amet'
+        ],
+    ];
+}
