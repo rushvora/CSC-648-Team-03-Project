413612a50b42a0f33400c3b5685dc90fc9c41316
--quiet <ahmeds@ip-172-31-19-160.us-west-1.compute.internal>
2017-05-08 05:49:16 +0000

    login with password authorization

--- my_app_name/src/Controller/AppController.php
+++ my_app_name/src/Controller/AppController.php
@@ -28,6 +28,42 @@ use Cake\Event\Event;
 class AppController extends Controller
 {
 
+public $components = array(
+    'Flash',
+    'Auth' => array(
+        'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+        'logoutRedirect' => array(
+            'controller' => 'pages',
+            'action' => 'display',
+            'home'
+        ),
+        'authenticate' => array(
+            'Form' => array(
+                'passwordHasher' => 'Blowfish'
+            )
+        ),
+        'authorize' => array('Controller') // Added this line
+    )
+);
+
+public $components = array(
+    'Flash',
+    'Auth' => array(
+        'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+        'logoutRedirect' => array(
+            'controller' => 'pages',
+            'action' => 'display',
+            'home'
+        ),
+        'authenticate' => array(
+            'Form' => array(
+                'passwordHasher' => 'Blowfish'
+            )
+        ),
+        'authorize' => array('Controller') // Added this line
+    )
+);
+
     /**
      * Initialization hook method.
      *
@@ -38,6 +74,17 @@ class AppController extends Controller
      * @return void
      */
 
+
+
+public function isAuthorized($user) {
+    // Admin can access every action
+    if (isset($user['role']) && $user['role'] === 'admin') {
+        return true;
+    }
+
+    // Default deny
+    return false;
+}
     public function initialize()
     {
         parent::initialize();
