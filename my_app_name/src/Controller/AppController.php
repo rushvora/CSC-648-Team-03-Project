52ed76e4ca90788120b29b1677afa80bd3414365
Mark Reynolds <mbreynolds147@gmail.com>
2017-05-08 23:27:12 +0000

    Revert AppController.
    Reverted AppController to b6f6241 combined with a63e77f

--- my_app_name/src/Controller/AppController.php
+++ my_app_name/src/Controller/AppController.php
@@ -28,42 +28,6 @@ use Cake\Event\Event;
 class AppController extends Controller
 {
 
-public $components = array(
-    'Flash',
-    'Auth' => array(
-        'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
-        'logoutRedirect' => array(
-            'controller' => 'pages',
-            'action' => 'display',
-            'home'
-        ),
-        'authenticate' => array(
-            'Form' => array(
-                'passwordHasher' => 'Blowfish'
-            )
-        ),
-        'authorize' => array('Controller') // Added this line
-    )
-);
-
-public $components = array(
-    'Flash',
-    'Auth' => array(
-        'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
-        'logoutRedirect' => array(
-            'controller' => 'pages',
-            'action' => 'display',
-            'home'
-        ),
-        'authenticate' => array(
-            'Form' => array(
-                'passwordHasher' => 'Blowfish'
-            )
-        ),
-        'authorize' => array('Controller') // Added this line
-    )
-);
-
     /**
      * Initialization hook method.
      *
@@ -74,17 +38,6 @@ public $components = array(
      * @return void
      */
 
-
-
-public function isAuthorized($user) {
-    // Admin can access every action
-    if (isset($user['role']) && $user['role'] === 'admin') {
-        return true;
-    }
-
-    // Default deny
-    return false;
-}
     public function initialize()
     {
         parent::initialize();
@@ -94,10 +47,10 @@ public function isAuthorized($user) {
             'authenticate' => [
                 'Form' => [
                     'fields' => ['username' => 'USERNAME', 'password' => 'PASSWORD'],
-                    'userModel' =>'Users'
+                    'userModel' => 'Users'
                 ]
             ],
-	        'loginRedirect' => [
+            'loginRedirect' => [
                 'controller' => 'pages',
                 'action' => 'display',
                 'home'
